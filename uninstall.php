dd63873fbdde17b8686a115c2f02c7d3047bf6d2
JDGrimes <jdg@codesymphony.co>
2014-02-03 16:20:11 -0500

    Add an uninstall.php file to clean up the database
    Fixes #1

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Clean up the database when the module is uninstalled.
+ *
+ * @package WordPoints_Beta_Tester
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'WORDPOINTS_UNINSTALL_MODULE' ) ) {
+	return;
+}
+
+delete_site_option( 'wordpoints_beta_version' );
